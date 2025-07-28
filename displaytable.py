9b70a87461f231a72dd039a56cc800a97d0006e1
lei-win <s10272765@connect.np.edu.sg>
2025-07-28 15:45:35 +0800

    This is my first commit

---
+++ displaytable.py
@@ -0,0 +1,35 @@
+import math
+
+# Displaying the headers
+print("Number  Square  Square root  English")
+
+# Manually calculating and printing the data for each number
+# For number 1
+num = 1
+square = num ** 2
+square_root = math.sqrt(num)
+print(num, square, round(square_root, 2), "One")
+
+# For number 2
+num = 2
+square = num ** 2
+square_root = math.sqrt(num)
+print(num, square, round(square_root, 2), "Two")
+
+# For number 3
+num = 3
+square = num ** 2
+square_root = math.sqrt(num)
+print(num, square, round(square_root, 2), "Three")
+
+# For number 4
+num = 4
+square = num ** 2
+square_root = math.sqrt(num)
+print(num, square, round(square_root, 2), "Four")
+
+# For number 5
+num = 5
+square = num ** 2
+square_root = math.sqrt(num)
+print(num, square, round(square_root, 2), "Five")
